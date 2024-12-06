6048cb7655ec09b26e2f6adf6d3ed5c41732dd1b
Ahercode <philipkelly407@gmail.com>
2024-12-06 07:04:23 +0000

    move all to remote

--- RateExchanger/Processes/RateProcessor.cs
+++ RateExchanger/Processes/RateProcessor.cs
@@ -19,7 +19,8 @@ public class RateProcessor
    
     public async Task ProcessRates( CancellationToken stoppingToken)
     {
-        var connectionString = _configuration.GetConnectionString("DefaultConnection");
+        var usdConnectionString = _configuration.GetConnectionString("USDConnection");
+        var ghcConnectionString = _configuration.GetConnectionString("GHCConnection");
         var url = _configuration["ApiSettings:apiUrl"];
         var key = _configuration["ApiSettings:key"];
         var host = _configuration["ApiSettings:host"];
@@ -43,7 +44,8 @@ public class RateProcessor
                 // log the data
                 _logger.LogInformation("Rate Time is {data}", data.rates.GHS);
                 _logger.LogInformation("Rate Time is {data}", data.time_update.time_utc);
-                using (SqlConnection connection = new SqlConnection(connectionString))
+                
+                using (SqlConnection connection = new SqlConnection(usdConnectionString))
                 {
                     await connection.OpenAsync(stoppingToken);
                 
@@ -53,7 +55,7 @@ public class RateProcessor
                     // convert the date to decimal
                     decimal audtDate = Convert.ToDecimal(date.ToString("yyyyMMdd"));
                     // get the current time and convert it to decimal
-                    decimal audtTime = Convert.ToDecimal(date.ToString("HHmmss"));
+                    decimal audtTime = Convert.ToDecimal(date.ToString("HHmmssff"));
                 
                     string dateTimeString = data.time_update.time_utc;
                     DateTime dateTime = DateTime.Parse(dateTimeString);
@@ -68,9 +70,43 @@ public class RateProcessor
                     string columns =
                         "HOMECUR, RATETYPE, SOURCECUR, RATEDATE, AUDTDATE, AUDTTIME, AUDTUSER, AUDTORG, RATE, SPREAD,DATEMATCH,RATEOPER";
                     string values =
-                        $"'GHC','SP','USD','{rateDate}', '{audtDate}', '{audtTime}', 'ADMIN', 'TARDAT', '{rate}', '0.0000000','3','1'";
+                        $"'GHC','SP','USD','{rateDate}', '{audtDate}', '{audtTime}', 'ADMIN', 'SYSUSD', '{rate}', '0.0000000','3','1'";
                     using (SqlCommand command =
-                           new SqlCommand($"INSERT INTO [TARKWAUSD].[dbo].[CSCRD] ({columns}) VALUES ({values})",
+                           new SqlCommand($"INSERT INTO [SYSTEMUSD].[dbo].[CSCRD] ({columns}) VALUES ({values})",
+                               connection))
+                    {
+                        await command.ExecuteNonQueryAsync(stoppingToken);
+                    }
+                }
+                
+                using (SqlConnection connection = new SqlConnection(ghcConnectionString))
+                {
+                    await connection.OpenAsync(stoppingToken);
+                
+                    // I need current date
+                    DateTime date = DateTime.Now;
+                
+                    // convert the date to decimal
+                    decimal audtDate = Convert.ToDecimal(date.ToString("yyyyMMdd"));
+                    // get the current time and convert it to decimal
+                    decimal audtTime = Convert.ToDecimal(date.ToString("HHmmssff"));
+                
+                    string dateTimeString = data.time_update.time_utc;
+                    DateTime dateTime = DateTime.Parse(dateTimeString);
+                    string dateString = dateTime.ToString("yyyyMMdd");
+                    decimal dateDecimal = Convert.ToDecimal(dateString);
+                
+                    decimal rateDate = dateDecimal;
+                    decimal rate = Convert.ToDecimal(data.rates.GHS);
+                
+                    _logger.LogInformation("Rate date is  {date}", rateDate);
+                
+                    string columns =
+                        "HOMECUR, RATETYPE, SOURCECUR, RATEDATE, AUDTDATE, AUDTTIME, AUDTUSER, AUDTORG, RATE, SPREAD,DATEMATCH,RATEOPER";
+                    string values =
+                        $"'USD','SP','GHC ','{rateDate}', '{audtDate}', '{audtTime}', 'ADMIN', 'SYSTEM', '{rate}', '0.0000000','3','2'";
+                    using (SqlCommand command =
+                           new SqlCommand($"INSERT INTO [SYSTEM].[dbo].[CSCRD] ({columns}) VALUES ({values})",
                                connection))
                     {
                         await command.ExecuteNonQueryAsync(stoppingToken);
